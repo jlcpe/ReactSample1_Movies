3eb2f7bc3b735634291b15c96aac7b95e6f4a386
JLJL <jaylordlubin@gmail.com>
2025-04-18 22:12:05 +0800

    added movie api

--- ReactTut/src/App.jsx
+++ ReactTut/src/App.jsx
@@ -1,29 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx'
+import MovieCard from './components/MovieCard.jsx';
 
-import { useState } from 'react'
-import './App.css'
-
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '💗': '🤍'}
-      </button>
-    </div>
-  )
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS ={
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+  }
 }
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      
+      if(data.Response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    }
+    catch(error){
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Error fetching movies, Please Try Again Later');
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
+  
+  
   return (
-    <div className="card-container">
-      <Card title="Star Wars" ratings={5} isCool={true}/>
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero_Banner" />
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
+
+          <section className="all-movies">
+            <h2 className="mt-[40px]">All Movies</h2>
+            
+            {isLoading ? ( 
+                <Spinner />
+              ) : errorMessage ? ( 
+                <p className="text-red-500">{errorMessage}</p>
+              ) : (
+                <ul>
+                  {movieList.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                  ))}
+                </ul>
+              )
+            }
+          </section>
+
+        </div>
+      </div>
+    </main>
+    
   )
 }
 
