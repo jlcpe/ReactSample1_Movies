cdc83d2f67af28a0d731157ceeb6c54c7261a75e
JLJL <jaylordlubin@gmail.com>
2025-04-18 17:14:39 +0800

    useState added

--- ReactTut/src/App.jsx
+++ ReactTut/src/App.jsx
@@ -1,10 +1,30 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {}
+const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
 
+  return (
+    <div className="card">
+      <h2>{title}</h2>
 
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '💗': '🤍'}
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" ratings={5} isCool={true}/>
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
+    </div>
+  )
+}
 
 export default App
